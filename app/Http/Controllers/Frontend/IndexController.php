73254ec13e118b58e23c3a478dd37e2e0a73690f
easylearningbd <elebdsoft@gmail.com>
2021-02-23 03:58:07 +0600

    76 Fontend Dynamic Main Sliders

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -7,13 +7,15 @@ use Illuminate\Http\Request;
 use Auth;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Slider;
 use Illuminate\Support\Facades\Hash;
 
 class IndexController extends Controller
 {
     public function index(){
+    	$sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
     	$categories = Category::orderBy('category_name_en','ASC')->get();
-    	return view('frontend.index',compact('categories'));
+    	return view('frontend.index',compact('categories','sliders'));
 
     }
 
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -860,43 +860,46 @@
       
       <!-- ============================================== CONTENT ============================================== -->
       <div class="col-xs-12 col-sm-12 col-md-9 homebanner-holder"> 
-        <!-- ========================================== SECTION – HERO ========================================= -->
+
+
+
+
+        <!-- === ========= SECTION – HERO ==== ======= -->
         
         <div id="hero">
           <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
-            <div class="item" style="background-image: url({{ asset('frontend/assets/images/sliders/01.jpg') }});">
-              <div class="container-fluid">
-                <div class="caption bg-color vertical-center text-left">
-                  <div class="slider-header fadeInDown-1">Top Brands</div>
-                  <div class="big-text fadeInDown-1"> New Collections </div>
-                  <div class="excerpt fadeInDown-2 hidden-xs"> <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span> </div>
-                  <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
-                </div>
-                <!-- /.caption --> 
-              </div>
-              <!-- /.container-fluid --> 
-            </div>
-            <!-- /.item -->
             
-            <div class="item" style="background-image: url({{ asset('frontend/assets/images/sliders/02.jpg') }});">
-              <div class="container-fluid">
-                <div class="caption bg-color vertical-center text-left">
-                  <div class="slider-header fadeInDown-1">Spring 2016</div>
-                  <div class="big-text fadeInDown-1"> Women <span class="highlight">Fashion</span> </div>
-                  <div class="excerpt fadeInDown-2 hidden-xs"> <span>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</span> </div>
-                  <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
-                </div>
-                <!-- /.caption --> 
-              </div>
-              <!-- /.container-fluid --> 
-            </div>
-            <!-- /.item --> 
+    @foreach($sliders as $slider)
+    <div class="item" style="background-image: url({{ asset($slider->slider_img) }});">
+      <div class="container-fluid">
+        <div class="caption bg-color vertical-center text-left">
+          
+          <div class="big-text fadeInDown-1">{{ $slider->title }} </div>
+          <div class="excerpt fadeInDown-2 hidden-xs"> <span>{{ $slider->description }}</span> </div>
+          <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
+        </div>
+        <!-- /.caption --> 
+      </div>
+      <!-- /.container-fluid --> 
+    </div>
+    <!-- /.item -->
+    @endforeach
+           
             
           </div>
           <!-- /.owl-carousel --> 
         </div>
         
-        <!-- ========================================= SECTION – HERO : END ========================================= --> 
+        <!-- ==== ===== SECTION – HERO : END === ============== --> 
+
+
+
+
+
+
+
+
+
         
         <!-- ============================================== INFO BOXES ============================================== -->
         <div class="info-boxes wow fadeInUp">
