78d5f69fbd470ede55981f8dea8e781a010591f3
easylearningbd <elebdsoft@gmail.com>
2021-02-25 03:51:44 +0600

    85 Hot Deals Product Show

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -20,7 +20,8 @@ class IndexController extends Controller
     	$categories = Category::orderBy('category_name_en','ASC')->get();
 
     	$featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
-    	return view('frontend.index',compact('categories','sliders','products','featured'));
+    	$hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(3)->get();
+    	return view('frontend.index',compact('categories','sliders','products','featured','hot_deals'));
 
     }
 
