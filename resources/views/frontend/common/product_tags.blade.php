0045fe42926dd2b38dcabf0c9c33dc7a8c329c95
easylearningbd <elebdsoft@gmail.com>
2021-02-26 02:58:40 +0600

    91 Product Tags Show

---
+++ resources/views/frontend/common/product_tags.blade.php
@@ -0,0 +1,38 @@
+@php
+
+$tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
+
+$tags_hin = App\Models\Product::groupBy('product_tags_hin')->select('product_tags_hin')->get();
+@endphp     
+
+
+
+
+     <div class="sidebar-widget product-tag wow fadeInUp">
+          <h3 class="section-title">Product tags</h3>
+          <div class="sidebar-widget-body outer-top-xs"> 
+
+<div class="tag-list"> 
+
+@if(session()->get('language') == 'hindi') 
+
+@foreach($tags_hin as $tag)
+<a class="item active" title="Phone" href="category.html">
+	{{ str_replace(',',' ',$tag->product_tags_hin)  }}</a> 
+@endforeach
+
+ @else 
+
+@foreach($tags_en as $tag)
+<a class="item active" title="Phone" href="category.html">
+	{{ str_replace(',',' ',$tag->product_tags_en)  }}</a> 
+@endforeach
+ 
+  @endif
+	
+	 </div>
+<!-- /.tag-list --> 
+</div>
+          <!-- /.sidebar-widget-body --> 
+        </div>
+        <!-- /.sidebar-widget --> 
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -250,17 +250,16 @@ Home Easy Online Shop
         </div>
         <!-- /.sidebar-widget --> 
         <!-- ============================================== SPECIAL OFFER : END ============================================== --> 
-        <!-- ============================================== PRODUCT TAGS ============================================== -->
-        <div class="sidebar-widget product-tag wow fadeInUp">
-          <h3 class="section-title">Product tags</h3>
-          <div class="sidebar-widget-body outer-top-xs">
-            <div class="tag-list"> <a class="item" title="Phone" href="category.html">Phone</a> <a class="item active" title="Vest" href="category.html">Vest</a> <a class="item" title="Smartphone" href="category.html">Smartphone</a> <a class="item" title="Furniture" href="category.html">Furniture</a> <a class="item" title="T-shirt" href="category.html">T-shirt</a> <a class="item" title="Sweatpants" href="category.html">Sweatpants</a> <a class="item" title="Sneaker" href="category.html">Sneaker</a> <a class="item" title="Toys" href="category.html">Toys</a> <a class="item" title="Rose" href="category.html">Rose</a> </div>
-            <!-- /.tag-list --> 
-          </div>
-          <!-- /.sidebar-widget-body --> 
-        </div>
-        <!-- /.sidebar-widget --> 
-        <!-- ============================================== PRODUCT TAGS : END ============================================== --> 
+
+
+
+        <!-- ===== ===== PRODUCT TAGS ==== ====== -->
+   @include('frontend.common.product_tags')
+        <!-- ==== ===== PRODUCT TAGS : END ======= ==== --> 
+
+
+
+
         <!-- ============================================== SPECIAL DEALS ============================================== -->
         
         <div class="sidebar-widget outer-bottom-small wow fadeInUp">
