ab5156bc227d3d73f3758ee724871a06fbbdf9c7
easylearningbd <elebdsoft@gmail.com>
2021-03-02 01:27:53 +0600

    108 Increase Modal Width.

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -112,7 +112,7 @@
 
             <div class="card" style="width: 18rem;">
 
-  <img src=" " class="card-img-top" alt="..." style="height: 200px; width: 180px;" id="pimage">
+  <img src=" " class="card-img-top" alt="..." style="height: 200px; width: 200px;" id="pimage">
   
 </div>
             
