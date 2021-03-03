e1b6c8387a05115b634d08359b71ca6a594af5c5
easylearningbd <elebdsoft@gmail.com>
2021-03-04 02:19:30 +0600

    127 Update Product Cart Url

--- routes/web.php
+++ routes/web.php
@@ -235,14 +235,15 @@ Route::get('/wishlist', [WishlistController::class, 'ViewWishlist'])->name('wish
 Route::get('/get-wishlist-product', [WishlistController::class, 'GetWishlistProduct']);
 
 Route::get('/wishlist-remove/{id}', [WishlistController::class, 'RemoveWishlistProduct']);
-
-Route::get('/mycart', [CartPageController::class, 'MyCart'])->name('mycart');
-
-Route::get('/get-cart-product', [CartPageController::class, 'GetCartProduct']);
-
-Route::get('/cart-remove/{rowId}', [CartPageController::class, 'RemoveCartProduct']);
-
-
-
+ 
 
 });
+
+
+
+ // My Cart Page All Routes
+Route::get('/mycart', [CartPageController::class, 'MyCart'])->name('mycart');
+
+Route::get('/user/get-cart-product', [CartPageController::class, 'GetCartProduct']);
+
+Route::get('/user/cart-remove/{rowId}', [CartPageController::class, 'RemoveCartProduct']);
