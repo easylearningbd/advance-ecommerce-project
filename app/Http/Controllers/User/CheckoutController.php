96cf5125d86fccbb5499542419bdd4d2f94f706a
easylearningbd <elebdsoft@gmail.com>
2021-03-12 04:21:10 +0600

    153  Checkout Page ¦ User Shipping Address Part 2

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -27,6 +27,26 @@ class CheckoutController extends Controller
 
 
     public function CheckoutStore(Request $request){
+    	// dd($request->all());
+    	$data = array();
+    	$data['shipping_name'] = $request->shipping_name;
+    	$data['shipping_email'] = $request->shipping_email;
+    	$data['shipping_phone'] = $request->shipping_phone;
+    	$data['post_code'] = $request->post_code;
+    	$data['division_id'] = $request->division_id;
+    	$data['district_id'] = $request->district_id;
+    	$data['state_id'] = $request->state_id;
+    	$data['notes'] = $request->notes;
+
+
+    	if ($request->payment_method == 'stripe') {
+    		return view('frontend.payment.stripe',compact('data'));
+    	}elseif ($request->payment_method == 'card') {
+    		return 'card';
+    	}else{
+            return 'cash';
+    	}
+    	 
 
     }// end mehtod. 
 
---
+++ resources/views/frontend/payment/stripe.blade.php
@@ -0,0 +1,4 @@
+<h1>This is a stripe Page</h1>
+<div>
+	{{ $data['shipping_name'] }}
+</div>
