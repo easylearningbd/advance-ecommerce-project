efbde033c64208e8f88ddc4fa13b9c0904ff2e7f
easylearningbd <elebdsoft@gmail.com>
2021-03-15 01:46:51 +0600

    154  Stripe Payment Gateway Page Design Part 1

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -8,6 +8,8 @@ use App\Models\ShipDivision;
 use App\Models\ShipDistrict;
 use App\Models\ShipState;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+
 class CheckoutController extends Controller
 {
     public function DistrictGetAjax($division_id){
@@ -37,10 +39,11 @@ class CheckoutController extends Controller
     	$data['district_id'] = $request->district_id;
     	$data['state_id'] = $request->state_id;
     	$data['notes'] = $request->notes;
+    	$cartTotal = Cart::total();
 
 
     	if ($request->payment_method == 'stripe') {
-    		return view('frontend.payment.stripe',compact('data'));
+    		return view('frontend.payment.stripe',compact('data','cartTotal'));
     	}elseif ($request->payment_method == 'card') {
     		return 'card';
     	}else{
