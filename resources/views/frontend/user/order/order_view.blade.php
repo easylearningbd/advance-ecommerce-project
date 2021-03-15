56b4226d0ade3d21ae4b661917fd24bed56f367b
easylearningbd <elebdsoft@gmail.com>
2021-03-16 03:37:14 +0600

    163 Read Orders In User Profile Part 2

--- resources/views/frontend/user/order/order_view.blade.php
+++ resources/views/frontend/user/order/order_view.blade.php
@@ -5,6 +5,96 @@
 	<div class="container">
 		<div class="row">
 			 @include('frontend.common.user_sidebar')
+       
+       <div class="col-md-2">
+       </div>
+
+       <div class="col-md-8">
+
+        <div class="table-responsive">
+          <table class="table">
+            <tbody>
+  
+              <tr style="background: #e2e2e2;">
+                <td class="col-md-1">
+                  <label for=""> Date</label>
+                </td>
+
+                <td class="col-md-3">
+                  <label for=""> Total</label>
+                </td>
+
+                <td class="col-md-3">
+                  <label for=""> Payment</label>
+                </td>
+
+
+                <td class="col-md-2">
+                  <label for=""> Invoice</label>
+                </td>
+
+                 <td class="col-md-2">
+                  <label for=""> Order</label>
+                </td>
+
+                 <td class="col-md-1">
+                  <label for=""> Action </label>
+                </td>
+                
+              </tr>
+
+
+              @foreach($orders as $order)
+       <tr>
+                <td class="col-md-1">
+                  <label for=""> {{ $order->order_date }}</label>
+                </td>
+
+                <td class="col-md-3">
+                  <label for=""> ${{ $order->amount }}</label>
+                </td>
+
+
+                 <td class="col-md-3">
+                  <label for=""> {{ $order->payment_method }}</label>
+                </td>
+
+                <td class="col-md-2">
+                  <label for=""> {{ $order->invoice_no }}</label>
+                </td>
+
+                 <td class="col-md-2">
+                  <label for=""> 
+                    <span class="badge badge-pill badge-warning" style="background: #418DB9;">{{ $order->status }} </span>
+
+                    </label>
+                </td>
+
+         <td class="col-md-1">
+          <a href="" class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> View</a>
+
+           <a href="" class="btn btn-sm btn-danger"><i class="fa fa-download" style="color: white;"></i> Invoice </a>
+          
+        </td>
+                
+              </tr>
+              @endforeach
+
+
+
+
+
+            </tbody>
+            
+          </table>
+          
+        </div>
+
+
+
+
+         
+       </div> <!-- / end col md 8 -->
 
 		 
 
