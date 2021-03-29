9712c8bb1f9975f92aa7ef79d0d3470c7942df80
easylearningbd <elebdsoft@gmail.com>
2021-03-30 05:30:36 +0600

    216 Login with New Admin User

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -13,20 +13,26 @@ class AdminProfileController extends Controller
 { 
    
 	public function AdminProfile(){
-		$adminData = Admin::find(1);
+
+		$id = Auth::user()->id;
+		$adminData = Admin::find($id);
 		return view('admin.admin_profile_view',compact('adminData'));
 	}
  
 
 	public function AdminProfileEdit(){
-		$editData = Admin::find(1);
+
+		$id = Auth::user()->id;
+		$editData = Admin::find($id);
 		return view('admin.admin_profile_edit',compact('editData'));
 
 	}
 
 
 	public function AdminProfileStore(Request $request){
-		$data = Admin::find(1);
+
+		$id = Auth::user()->id;
+		$data = Admin::find($id);
 		$data->name = $request->name;
 		$data->email = $request->email;
 
@@ -65,9 +71,9 @@ class AdminProfileController extends Controller
 			'password' => 'required|confirmed',
 		]);
 
-		$hashedPassword = Admin::find(1)->password;
+		$hashedPassword = Auth::user()->password;
 		if (Hash::check($request->oldpassword,$hashedPassword)) {
-			$admin = Admin::find(1);
+			$admin = Admin::find(Auth::id());
 			$admin->password = Hash::make($request->password);
 			$admin->save();
 			Auth::logout();
