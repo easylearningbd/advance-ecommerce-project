4802eadd5020cc54fcbc67eb0f918fb7f0615130
easylearningbd <elebdsoft@gmail.com>
2021-05-06 02:39:10 +0600

    236 Upload Digital Product Part 2

--- resources/views/frontend/user/order/order_details.blade.php
+++ resources/views/frontend/user/order/order_details.blade.php
@@ -158,6 +158,10 @@
                 <td class="col-md-1">
                   <label for=""> Price </label>
                 </td>
+
+                 <td class="col-md-1">
+                  <label for=""> Download </label>
+                </td>
                 
               </tr>
 
@@ -192,6 +196,31 @@
           <td class="col-md-2">
                   <label for=""> ${{ $item->price }}  ( $ {{ $item->price * $item->qty}} ) </label>
                 </td>
+
+
+@php 
+
+$file = App\Models\Product::where('id',$item->product_id)->first();
+@endphp
+
+             <td class="col-md-1">
+              @if($order->status == 'pending')  
+              <strong>
+ <span class="badge badge-pill badge-success" style="background: #418DB9;"> No File</span>  </strong> 
+                
+              @elseif($order->status == 'confirm')  
+
+<a target="_blank" href="{{ asset('upload/pdf/'.$file->digital_file) }}">  
+  <strong>
+ <span class="badge badge-pill badge-success" style="background: #FF0000;"> Download Ready</span>  </strong> </a> 
+              @endif
+
+
+                </td>
+
+
+
+
                 
               </tr>
               @endforeach
