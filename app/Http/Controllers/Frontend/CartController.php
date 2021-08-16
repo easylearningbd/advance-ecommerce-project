be29af5dccc780ee9788e09af89f18491f00286b
saber <saber.tabatabaee@gmail.com>
2021-08-16 16:55:20 +0430

    fix: checkout links and test

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -14,135 +14,139 @@ use App\Models\Coupon;
 use Illuminate\Support\Facades\Session;
 
 use App\Models\ShipDivision;
- 
+
 class CartController extends Controller
 {
-    public function AddToCart(Request $request, $id){
+    public function AddToCart(Request $request, $id)
+    {
 
-         if (Session::has('coupon')) {
-           Session::forget('coupon');
+        if (Session::has('coupon')) {
+            Session::forget('coupon');
         }
-          
-    	$product = Product::findOrFail($id);
 
-    	if ($product->discount_price == NULL) {
-    		Cart::add([
-    			'id' => $id, 
-    			'name' => $request->product_name, 
-    			'qty' => $request->quantity, 
-    			'price' => $product->selling_price,
-    			'weight' => 1, 
-    			'options' => [
-    				'image' => $product->product_thambnail,
-    				'color' => $request->color,
-    				'size' => $request->size,
-    			], 
-    		]);
+        $product = Product::findOrFail($id);
 
-    		return response()->json(['success' => 'Successfully Added on Your Cart']);
-    		 
-    	}else{
+        if ($product->discount_price == NULL) {
+            Cart::add([
+                'id' => $id,
+                'name' => $request->product_name,
+                'qty' => $request->quantity,
+                'price' => $product->selling_price,
+                'weight' => 1,
+                'options' => [
+                    'image' => $product->product_thambnail,
+                    'color' => $request->color,
+                    'size' => $request->size,
+                ],
+            ]);
 
-    		Cart::add([
-    			'id' => $id, 
-    			'name' => $request->product_name, 
-    			'qty' => $request->quantity, 
-    			'price' => $product->discount_price,
-    			'weight' => 1, 
-    			'options' => [
-    				'image' => $product->product_thambnail,
-    				'color' => $request->color,
-    				'size' => $request->size,
-    			],
-    		]);
-    		return response()->json(['success' => 'Successfully Added on Your Cart']);
-    	}
+            return response()->json(['success' => 'Successfully Added on Your Cart']);
 
-    } // end mehtod 
+        } else {
+
+            Cart::add([
+                'id' => $id,
+                'name' => $request->product_name,
+                'qty' => $request->quantity,
+                'price' => $product->discount_price,
+                'weight' => 1,
+                'options' => [
+                    'image' => $product->product_thambnail,
+                    'color' => $request->color,
+                    'size' => $request->size,
+                ],
+            ]);
+            return response()->json(['success' => 'Successfully Added on Your Cart']);
+        }
+
+    } // end mehtod
 
 
     // Mini Cart Section
-    public function AddMiniCart(){
+    public function AddMiniCart()
+    {
 
-    	$carts = Cart::content();
-    	$cartQty = Cart::count();
-    	$cartTotal = Cart::total();
+        $carts = Cart::content();
+        $cartQty = Cart::count();
+        $cartTotal = Cart::total();
 
-    	return response()->json(array(
-    		'carts' => $carts,
-    		'cartQty' => $cartQty,
-    		'cartTotal' => round($cartTotal),
+        return response()->json(array(
+            'carts' => $carts,
+            'cartQty' => $cartQty,
+            'cartTotal' => round($cartTotal),
 
-    	));
-    } // end method 
-
-
-/// remove mini cart 
-    public function RemoveMiniCart($rowId){
-    	Cart::remove($rowId);
-    	return response()->json(['success' => 'Product Remove from Cart']);
-
-    } // end mehtod 
+        ));
+    } // end method
 
 
-    // add to wishlist mehtod 
+/// remove mini cart
+    public function RemoveMiniCart($rowId)
+    {
+        Cart::remove($rowId);
+        return response()->json(['success' => 'Product Remove from Cart']);
 
-    public function AddToWishlist(Request $request, $product_id){
+    } // end mehtod
+
+
+    // add to wishlist mehtod
+
+    public function AddToWishlist(Request $request, $product_id)
+    {
 
         if (Auth::check()) {
 
-            $exists = Wishlist::where('user_id',Auth::id())->where('product_id',$product_id)->first();
+            $exists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
 
             if (!$exists) {
-               Wishlist::insert([
-                'user_id' => Auth::id(), 
-                'product_id' => $product_id, 
-                'created_at' => Carbon::now(), 
-            ]);
-           return response()->json(['success' => 'Successfully Added On Your Wishlist']);
+                Wishlist::insert([
+                    'user_id' => Auth::id(),
+                    'product_id' => $product_id,
+                    'created_at' => Carbon::now(),
+                ]);
+                return response()->json(['success' => 'Successfully Added On Your Wishlist']);
 
-            }else{
+            } else {
 
                 return response()->json(['error' => 'This Product has Already on Your Wishlist']);
 
-            }            
-            
-        }else{
+            }
+
+        } else {
 
             return response()->json(['error' => 'At First Login Your Account']);
 
         }
 
-    } // end method 
+    } // end method
 
 
+    public function CouponApply(Request $request)
+    {
 
-
-    public function CouponApply(Request $request){
-
-        $coupon = Coupon::where('coupon_name',$request->coupon_name)->where('coupon_validity','>=',Carbon::now()->format('Y-m-d'))->first();
+        $coupon = Coupon::where('coupon_name', $request->coupon_name)->where('coupon_validity', '>=', Carbon::now()->format('Y-m-d'))->first();
         if ($coupon) {
 
-            Session::put('coupon',[
+            Session::put('coupon', [
                 'coupon_name' => $coupon->coupon_name,
                 'coupon_discount' => $coupon->coupon_discount,
-                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
-                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100)  
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount / 100),
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount / 100)
             ]);
- 
+
             return response()->json(array(
                 'validity' => true,
                 'success' => 'Coupon Applied Successfully'
             ));
-            
-        }else{
+
+        } else {
             return response()->json(['error' => 'Invalid Coupon']);
         }
 
-    } // end method 
+    } // end method
 
 
-    public function CouponCalculation(){
+    public function CouponCalculation()
+    {
 
         if (Session::has('coupon')) {
             return response()->json(array(
@@ -152,65 +156,62 @@ class CartController extends Controller
                 'discount_amount' => session()->get('coupon')['discount_amount'],
                 'total_amount' => session()->get('coupon')['total_amount'],
             ));
-        }else{
+        } else {
             return response()->json(array(
                 'total' => Cart::total(),
             ));
 
         }
-    } // end method 
+    } // end method
 
 
- // Remove Coupon 
-    public function CouponRemove(){
+    // Remove Coupon
+    public function CouponRemove()
+    {
         Session::forget('coupon');
         return response()->json(['success' => 'Coupon Remove Successfully']);
     }
 
 
-
- // Checkout Method 
-    public function CheckoutCreate(){
+    // Checkout Method
+    public function CheckoutCreate()
+    {
 
         if (Auth::check()) {
             if (Cart::total() > 0) {
 
-        $carts = Cart::content();
-        $cartQty = Cart::count();
-        $cartTotal = Cart::total();
+                $carts = Cart::content();
+                $cartQty = Cart::count();
+                $cartTotal = Cart::total();
 
-        $divisions = ShipDivision::orderBy('division_name','ASC')->get();
-        return view('frontend.checkout.checkout_view',compact('carts','cartQty','cartTotal','divisions'));
-                
-            }else{
+                $divisions = ShipDivision::orderBy('division_name', 'ASC')->get();
+//        dd($cartQty,$carts,$cartTotal,$divisions);
+                return view('frontend.checkout.checkout_view', compact('carts', 'cartQty', 'cartTotal', 'divisions'));
 
-            $notification = array(
-            'message' => 'Shopping At list One Product',
-            'alert-type' => 'error'
-        );
+            } else {
 
-        return redirect()->to('/')->with($notification);
+                $notification = array(
+                    'message' => 'Shopping At list One Product',
+                    'alert-type' => 'error'
+                );
+
+                return redirect()->to('/')->with($notification);
 
             }
 
-            
-        }else{
 
-             $notification = array(
-            'message' => 'You Need to Login First',
-            'alert-type' => 'error'
-        );
+        } else {
 
-        return redirect()->route('login')->with($notification);
+            $notification = array(
+                'message' => 'You Need to Login First',
+                'alert-type' => 'error'
+            );
+
+            return redirect()->route('login')->with($notification);
 
         }
 
-    } // end method 
-
-
-
-
+    } // end method
 
 
 }
- 
--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -1,5 +1,5 @@
-<header class="header-style-1"> 
-  
+<header class="header-style-1">
+
   <!-- ============================================== TOP MENU ============================================== -->
   <div class="top-bar animate-dropdown">
     <div class="container">
@@ -16,20 +16,20 @@
  <li><a href="" type="button" data-toggle="modal" data-target="#ordertraking"><i class="icon fa fa-check"></i>Order Traking</a></li>
 
             <li>
-    
+
 
    @auth
    <a href="{{ route('login') }}"><i class="icon fa fa-user"></i>User Profile</a>
    @else
    <a href="{{ route('login') }}"><i class="icon fa fa-lock"></i>Login/Register</a>
    @endauth
-              
+
 
             </li>
           </ul>
         </div>
         <!-- /.cnt-account -->
-        
+
         <div class="cnt-block">
           <ul class="list-unstyled list-inline">
             <li class="dropdown dropdown-small"> <a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown"><span class="value">USD </span><b class="caret"></b></a>
@@ -43,24 +43,24 @@
 @if(session()->get('language') == 'hindi') भाषा: हिन्दी @else Language @endif
   </span><b class="caret"></b></a>
               <ul class="dropdown-menu">
-         @if(session()->get('language') == 'hindi')       
+         @if(session()->get('language') == 'hindi')
         <li><a href="{{ route('english.language') }}">English</a></li>
         @else
         <li><a href="{{ route('hindi.language') }}">हिन्दी</a></li>
-         @endif      
+         @endif
               </ul>
             </li>
           </ul>
-          <!-- /.list-unstyled --> 
+          <!-- /.list-unstyled -->
         </div>
         <!-- /.cnt-cart -->
         <div class="clearfix"></div>
       </div>
-      <!-- /.header-top-inner --> 
+      <!-- /.header-top-inner -->
     </div>
-    <!-- /.container --> 
+    <!-- /.container -->
   </div>
-  <!-- /.header-top --> 
+  <!-- /.header-top -->
   <!-- ============================================== TOP MENU : END ============================================== -->
   <div class="main-header">
     <div class="container">
@@ -74,25 +74,25 @@
 
           <!-- ============================================================= LOGO ============================================================= -->
           <div class="logo"> <a href="{{ url('/') }}"> <img src="{{ asset($setting->logo) }}" alt="logo"> </a> </div>
-          <!-- /.logo --> 
+          <!-- /.logo -->
           <!-- ============================================================= LOGO : END ============================================================= --> </div>
         <!-- /.logo-holder -->
-        
-        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder"> 
-          <!-- /.contact-row --> 
+
+        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder">
+          <!-- /.contact-row -->
           <!-- ============================================================= SEARCH AREA ============================================================= -->
           <div class="search-area">
             <form method="post" action="{{ route('product.search') }}">
               @csrf
               <div class="control-group">
                 <ul class="categories-filter animate-dropdown">
-                  <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
+                  <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category">Categories <b class="caret"></b></a>
                     <ul class="dropdown-menu" role="menu" >
                       <li class="menu-header">Computer</li>
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Clothing</a></li>
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Electronics</a></li>
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Shoes</a></li>
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Watches</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category">- Clothing</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category">- Electronics</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category">- Shoes</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category">- Watches</a></li>
                     </ul>
                   </li>
                 </ul>
@@ -101,20 +101,20 @@
             </form>
             <div id="searchProducts"></div>
           </div>
-          <!-- /.search-area --> 
+          <!-- /.search-area -->
           <!-- ============================================================= SEARCH AREA : END ============================================================= --> </div>
         <!-- /.top-search-holder -->
-        
-        <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row"> 
-          
+
+        <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
+
 
           <!-- ===== === SHOPPING CART DROPDOWN ===== == -->
-          
+
           <div class="dropdown dropdown-cart"> <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
             <div class="items-cart-inner">
               <div class="basket"> <i class="glyphicon glyphicon-shopping-cart"></i> </div>
     <div class="basket-item-count"><span class="count" id="cartQty"> </span></div>
-              <div class="total-price-basket"> <span class="lbl">cart -</span> 
+              <div class="total-price-basket"> <span class="lbl">cart -</span>
                 <span class="total-price"> <span class="sign">$</span>
                 <span class="value" id="cartSubTotal"> </span> </span> </div>
             </div>
@@ -124,9 +124,9 @@
          <!--   // Mini Cart Start with Ajax -->
 
          <div id="miniCart">
-           
+
          </div>
- 
+
 <!--   // End Mini Cart Start with Ajax -->
 
 
@@ -134,33 +134,33 @@
                   <div class="pull-right"> <span class="text">Sub Total :</span>
                     <span class='price'  id="cartSubTotal">  </span> </div>
                   <div class="clearfix"></div>
-                  <a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a> </div>
-                <!-- /.cart-total--> 
-                
+                  <a href="checkout" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a> </div>
+                <!-- /.cart-total-->
+
               </li>
             </ul>
-            <!-- /.dropdown-menu--> 
+            <!-- /.dropdown-menu-->
           </div>
-          <!-- /.dropdown-cart --> 
-          
+          <!-- /.dropdown-cart -->
+
           <!-- == === SHOPPING CART DROPDOWN : END=== === --> </div>
 
-        <!-- /.top-cart-row --> 
+        <!-- /.top-cart-row -->
       </div>
-      <!-- /.row --> 
-      
+      <!-- /.row -->
+
     </div>
-    <!-- /.container --> 
-    
+    <!-- /.container -->
+
   </div>
-  <!-- /.main-header --> 
-  
+  <!-- /.main-header -->
+
   <!-- ============================================== NAVBAR ============================================== -->
   <div class="header-nav animate-dropdown">
     <div class="container">
       <div class="yamm navbar navbar-default" role="navigation">
         <div class="navbar-header">
-       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> 
+       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
        <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         </div>
         <div class="nav-bg-class">
@@ -178,7 +178,7 @@
 
 
  @foreach($categories as $category)
-  <li class="dropdown yamm mega-menu"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">
+  <li class="dropdown yamm mega-menu"> <a href="home" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">
     @if(session()->get('language') == 'hindi') {{ $category->category_name_hin }} @else {{ $category->category_name_en }} @endif
     </a>
     <ul class="dropdown-menu container">
@@ -203,24 +203,24 @@
     <!--   // Get SubSubCategory Table Data -->
   @php
   $subsubcategories = App\Models\SubSubCategory::where('subcategory_id',$subcategory->id)->orderBy('subsubcategory_name_en','ASC')->get();
-  @endphp          
+  @endphp
 
    @foreach($subsubcategories as $subsubcategory)
               <ul class="links">
                 <li><a href="{{ url('subsubcategory/product/'.$subsubcategory->id.'/'.$subsubcategory->subsubcategory_slug_en ) }}">
 @if(session()->get('language') == 'hindi') {{ $subsubcategory->subsubcategory_name_hin }} @else {{ $subsubcategory->subsubcategory_name_en }} @endif
                   </a></li>
-                
+
               </ul>
      @endforeach <!-- // End SubSubCategory Foreach -->
 
             </div>
             <!-- /.col -->
             @endforeach <!-- // End SubCategory Foreach -->
-           
-            
+
+
             <div class="col-xs-12 col-sm-6 col-md-4 col-menu banner-image"> <img class="img-responsive" src="{{ asset('frontend/assets/images/banners/top-menu-banner.jpg') }}" alt=""> </div>
-            <!-- /.yamm-content --> 
+            <!-- /.yamm-content -->
           </div>
         </div>
       </li>
@@ -230,7 +230,7 @@
 
 
      <li> <a href="{{ route('shop.page') }}">Shop</a> </li>
-               
+
                 <li class="dropdown  navbar-right special-menu"> <a href="#">Todays offer</a> </li>
 
  <li class="dropdown  navbar-right special-menu"> <a href="{{ route('home.blog') }}">Blog</a> </li>
@@ -240,21 +240,21 @@
               <!-- /.navbar-nav -->
               <div class="clearfix"></div>
             </div>
-            <!-- /.nav-outer --> 
+            <!-- /.nav-outer -->
           </div>
-          <!-- /.navbar-collapse --> 
-          
+          <!-- /.navbar-collapse -->
+
         </div>
-        <!-- /.nav-bg-class --> 
+        <!-- /.nav-bg-class -->
       </div>
-      <!-- /.navbar-default --> 
+      <!-- /.navbar-default -->
     </div>
-    <!-- /.container-class --> 
-    
+    <!-- /.container-class -->
+
   </div>
-  <!-- /.header-nav --> 
-  <!-- ============================================== NAVBAR : END ============================================== --> 
-  
+  <!-- /.header-nav -->
+  <!-- ============================================== NAVBAR : END ============================================== -->
+
 <!-- Order Traking Modal -->
 <div class="modal fade" id="ordertraking" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -266,31 +266,31 @@
         </button>
       </div>
       <div class="modal-body">
-         
+
         <form method="post" action="{{ route('order.tracking') }}">
           @csrf
          <div class="modal-body">
           <label>Invoice Code</label>
-          <input type="text" name="code" required="" class="form-control" placeholder="Your Order Invoice Number">           
+          <input type="text" name="code" required="" class="form-control" placeholder="Your Order Invoice Number">
          </div>
 
          <button class="btn btn-danger" type="submit" style="margin-left: 17px;"> Track Now </button>
-          
-        </form> 
+
+        </form>
 
 
       </div>
-       
+
     </div>
   </div>
 </div>
- 
+
 
 </header>
 
 
 <style>
-  
+
 .search-area{
   position: relative;
 }
