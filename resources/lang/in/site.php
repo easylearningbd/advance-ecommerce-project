52914720703eb7b1e0946dc2ddf016c550a4fc90
saber <saber.tabatabaee@gmail.com>
2021-08-18 00:06:49 +0430

    fix: add lang persian as hindi then right to left with bootstrap rtl version 4 add translate files

---
+++ resources/lang/in/site.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'search_here' => 'جستجو...',
+    'categories' => 'دسته بندی',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
+
+
