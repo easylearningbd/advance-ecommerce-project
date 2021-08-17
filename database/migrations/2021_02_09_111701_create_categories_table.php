d1187ba19cbf7c4d6957955fd6b64671081057c0
Saber tabatabaee yazdi <saber13812002@users.noreply.github.com>
2021-08-17 11:31:33 +0430

    Update 2021_02_09_111701_create_categories_table.php

--- database/migrations/2021_02_09_111701_create_categories_table.php
+++ database/migrations/2021_02_09_111701_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreateCategoriesTable extends Migration
 {
@@ -21,6 +22,7 @@ class CreateCategoriesTable extends Migration
             $table->string('category_slug_hin');
             $table->string('category_icon'); 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
