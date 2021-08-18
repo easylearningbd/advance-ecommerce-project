ddcb3f4ac41eabce632b5399ebcc1e0b15c393a6
saber <saber.tabatabaee@gmail.com>
2021-08-18 16:32:23 +0430

    fix: reformat code

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,17 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
 
-     public function product(){
-    	return $this->belongsTo(Product::class,'product_id','id');
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 
- 
-     public function user(){
-    	return $this->belongsTo(User::class,'user_id','id');
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
- 
 
 }
- 
