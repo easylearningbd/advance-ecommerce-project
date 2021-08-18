5577e5ab22e6d749737ade20a927d382703740bb
saber <saber.tabatabaee@gmail.com>
2021-08-18 23:52:42 +0430

    fix: search bar in header width should 6-col and cart box should col-3

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -83,7 +83,7 @@
                 </div>
                 <!-- /.logo-holder -->
 
-                <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder">
+                <div class="col-xs-12 col-sm-12 col-md-6 top-search-holder">
                     <!-- /.contact-row -->
                     <!-- ============================================================= SEARCH AREA ============================================================= -->
                     <div class="search-area">
@@ -122,7 +122,7 @@
                 </div>
                 <!-- /.top-search-holder -->
 
-                <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
+                <div class="col-xs-12 col-sm-12 col-md-3 animate-dropdown top-cart-row">
 
 
                     <!-- ===== === SHOPPING CART DROPDOWN ===== == -->
