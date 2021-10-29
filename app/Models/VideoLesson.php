c6f3aaeb911d677275a502ef915e161a8923cb4a
saber <saber.tabatabaee@gmail.com>
2021-10-29 15:08:30 +0330

    feat: product video lesson model added

--- app/Models/VideoLesson.php
+++ app/Models/VideoLesson.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class MultiMedia extends Model
+class VideoLesson extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
