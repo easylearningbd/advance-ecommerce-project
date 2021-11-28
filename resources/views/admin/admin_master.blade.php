9919c6bf597a65c5606315c473b4b4739734d5b9
saber13812002 <saber.tabatabaee@gmail.com>
2021-11-28 16:33:17 +0330

    move all assets into assets folder and

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="{{ asset('backend/images/favicon.ico') }}">
+    <link rel="icon" href="{{ asset('assets/backend/images/favicon.ico') }}">
 
     <title>Green Ecommerce Admin - Dashboard</title>
 
 	<!-- Vendors Style-->
-	<link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/backend/css/vendors_css.css') }}">
 
 	<!-- Style-->
-	<link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
-	<link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/backend/css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/backend/css/skin_color.css') }}">
 
    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
@@ -53,14 +53,14 @@
 
 
 	<!-- Vendor JS -->
-	<script src="{{ asset('backend/js/vendors.min.js') }}"></script>
+	<script src="{{ asset('assets/backend/js/vendors.min.js') }}"></script>
     <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
 
   <script src="{{ asset('../assets/vendor_components/datatable/datatables.min.js') }}"></script>
-  <script src="{{ asset('backend/js/pages/data-table.js') }}"></script>
+  <script src="{{ asset('assets/backend/js/pages/data-table.js') }}"></script>
 
 
 <!-- /// Tgas Input Script -->
@@ -69,12 +69,12 @@
  <!-- // CK EDITOR  -->
   <script src="{{ asset('../assets/vendor_components/ckeditor/ckeditor.js') }}"></script>
   <script src="{{ asset('../assets/vendor_plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.js') }}"></script>
-  <script src="{{ asset('backend/js/pages/editor.js') }}"></script>
+  <script src="{{ asset('assets/backend/js/pages/editor.js') }}"></script>
 
 
 	<!-- Sunny Admin App -->
-	<script src="{{ asset('backend/js/template.js') }}"></script>
-	<script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
+	<script src="{{ asset('assets/backend/js/template.js') }}"></script>
+	<script src="{{ asset('assets/backend/js/pages/dashboard.js') }}"></script>
 
 
 
@@ -106,7 +106,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
- <script src="{{ asset('backend/js/code.js') }}"></script>
+ <script src="{{ asset('assets/backend/js/code.js') }}"></script>
 
 
 </body>
