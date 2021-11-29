040d573b771f23005c06e114439a0680445dd151
saber <saber.tabatabaee@gmail.com>
2021-11-30 00:05:57 +0330

    fix: storage problem

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'parswebserver' => [
+            'driver' => 'local',
+            'root' => env('STORAGE_PATH'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
