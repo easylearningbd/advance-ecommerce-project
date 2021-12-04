80e575848b4d041d4f14b751faf3e613f967601f
saber13812002 <saber.tabatabaee@gmail.com>
2021-12-04 15:34:25 +0330

    feat/store order with order item

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -41,6 +41,7 @@ class OrderController extends Controller
         $entries = $entries->get();
         return response(new OrderResourceCollection(['data' => $entries, 'count' => $count]));
     }
+
     /**
      * @OA\Get(path="/api/orders/{orderId}",
      *   tags={"Orders"},
@@ -79,173 +80,178 @@ class OrderController extends Controller
     }
 
     // Pending Orders
-	public function PendingOrders(){
-		$orders = Order::where('status','pending')->orderBy('id','DESC')->get();
-		return view('backend.orders.pending_orders',compact('orders'));
+    public function PendingOrders()
+    {
+        $orders = Order::where('status', 'pending')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.pending_orders', compact('orders'));
 
-	}
+    }
 
 
-	// Pending Order Details
-	public function PendingOrdersDetails($order_id){
+    // Pending Order Details
+    public function PendingOrdersDetails($order_id)
+    {
 
-		$order = Order::with('division','district','state','user')->where('id',$order_id)->first();
-    	$orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
-    	return view('backend.orders.pending_orders_details',compact('order','orderItem'));
+        $order = Order::with('division', 'district', 'state', 'user')->where('id', $order_id)->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $order_id)->orderBy('id', 'DESC')->get();
+        return view('backend.orders.pending_orders_details', compact('order', 'orderItem'));
 
-	}
+    }
 
 
+    // Confirmed Orders
+    public function ConfirmedOrders()
+    {
+        $orders = Order::where('status', 'confirm')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.confirmed_orders', compact('orders'));
 
-	// Confirmed Orders
-	public function ConfirmedOrders(){
-		$orders = Order::where('status','confirm')->orderBy('id','DESC')->get();
-		return view('backend.orders.confirmed_orders',compact('orders'));
+    }
 
-	}
 
+    // Processing Orders
+    public function ProcessingOrders()
+    {
+        $orders = Order::where('status', 'processing')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.processing_orders', compact('orders'));
 
-	// Processing Orders
-	public function ProcessingOrders(){
-		$orders = Order::where('status','processing')->orderBy('id','DESC')->get();
-		return view('backend.orders.processing_orders',compact('orders'));
+    }
 
-	}
 
+    // Picked Orders
+    public function PickedOrders()
+    {
+        $orders = Order::where('status', 'picked')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.picked_orders', compact('orders'));
 
-		// Picked Orders
-	public function PickedOrders(){
-		$orders = Order::where('status','picked')->orderBy('id','DESC')->get();
-		return view('backend.orders.picked_orders',compact('orders'));
+    }
 
-	}
 
+    // Shipped Orders
+    public function ShippedOrders()
+    {
+        $orders = Order::where('status', 'shipped')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.shipped_orders', compact('orders'));
 
+    }
 
-			// Shipped Orders
-	public function ShippedOrders(){
-		$orders = Order::where('status','shipped')->orderBy('id','DESC')->get();
-		return view('backend.orders.shipped_orders',compact('orders'));
 
-	}
+    // Delivered Orders
+    public function DeliveredOrders()
+    {
+        $orders = Order::where('status', 'delivered')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.delivered_orders', compact('orders'));
 
+    }
 
-			// Delivered Orders
-	public function DeliveredOrders(){
-		$orders = Order::where('status','delivered')->orderBy('id','DESC')->get();
-		return view('backend.orders.delivered_orders',compact('orders'));
 
-	}
+    // Cancel Orders
+    public function CancelOrders()
+    {
+        $orders = Order::where('status', 'cancel')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.cancel_orders', compact('orders'));
 
+    }
 
-				// Cancel Orders
-	public function CancelOrders(){
-		$orders = Order::where('status','cancel')->orderBy('id','DESC')->get();
-		return view('backend.orders.cancel_orders',compact('orders'));
 
-	}
+    public function PendingToConfirm($order_id)
+    {
 
+        Order::findOrFail($order_id)->update(['status' => 'confirm']);
 
+        $notification = array(
+            'message' => 'Order Confirm Successfully',
+            'alert-type' => 'success'
+        );
 
+        return redirect()->route('pending-orders')->with($notification);
 
-	public function PendingToConfirm($order_id){
 
-      Order::findOrFail($order_id)->update(['status' => 'confirm']);
+    }
 
-      $notification = array(
-			'message' => 'Order Confirm Successfully',
-			'alert-type' => 'success'
-		);
 
-		return redirect()->route('pending-orders')->with($notification);
+    public function ConfirmToProcessing($order_id)
+    {
 
+        Order::findOrFail($order_id)->update(['status' => 'processing']);
 
-	}
+        $notification = array(
+            'message' => 'Order Processing Successfully',
+            'alert-type' => 'success'
+        );
 
+        return redirect()->route('confirmed-orders')->with($notification);
 
 
+    }
 
 
-	public function ConfirmToProcessing($order_id){
+    public function ProcessingToPicked($order_id)
+    {
 
-      Order::findOrFail($order_id)->update(['status' => 'processing']);
+        Order::findOrFail($order_id)->update(['status' => 'picked']);
 
-      $notification = array(
-			'message' => 'Order Processing Successfully',
-			'alert-type' => 'success'
-		);
+        $notification = array(
+            'message' => 'Order Picked Successfully',
+            'alert-type' => 'success'
+        );
 
-		return redirect()->route('confirmed-orders')->with($notification);
+        return redirect()->route('processing-orders')->with($notification);
 
 
-	}
+    }
 
 
+    public function PickedToShipped($order_id)
+    {
 
-		public function ProcessingToPicked($order_id){
+        Order::findOrFail($order_id)->update(['status' => 'shipped']);
 
-      Order::findOrFail($order_id)->update(['status' => 'picked']);
+        $notification = array(
+            'message' => 'Order Shipped Successfully',
+            'alert-type' => 'success'
+        );
 
-      $notification = array(
-			'message' => 'Order Picked Successfully',
-			'alert-type' => 'success'
-		);
+        return redirect()->route('picked-orders')->with($notification);
 
-		return redirect()->route('processing-orders')->with($notification);
 
+    }
 
-	}
 
+    public function ShippedToDelivered($order_id)
+    {
 
-	 public function PickedToShipped($order_id){
+        $product = OrderItem::where('order_id', $order_id)->get();
+        foreach ($product as $item) {
+            Product::where('id', $item->product_id)
+                ->update(['product_qty' => DB::raw('product_qty-' . $item->qty)]);
+        }
 
-      Order::findOrFail($order_id)->update(['status' => 'shipped']);
+        Order::findOrFail($order_id)->update(['status' => 'delivered']);
 
-      $notification = array(
-			'message' => 'Order Shipped Successfully',
-			'alert-type' => 'success'
-		);
+        $notification = array(
+            'message' => 'Order Delivered Successfully',
+            'alert-type' => 'success'
+        );
 
-		return redirect()->route('picked-orders')->with($notification);
+        return redirect()->route('shipped-orders')->with($notification);
 
 
-	}
+    }
 
 
-	 public function ShippedToDelivered($order_id){
+    public function AdminInvoiceDownload($order_id)
+    {
 
-	 $product = OrderItem::where('order_id',$order_id)->get();
-	 foreach ($product as $item) {
-	 	Product::where('id',$item->product_id)
-	 			->update(['product_qty' => DB::raw('product_qty-'.$item->qty)]);
-	 }
+        $order = Order::with('division', 'district', 'state', 'user')->where('id', $order_id)->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $order_id)->orderBy('id', 'DESC')->get();
 
-      Order::findOrFail($order_id)->update(['status' => 'delivered']);
+        $pdf = PDF::loadView('backend.orders.order_invoice', compact('order', 'orderItem'))->setPaper('a4')->setOptions([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+        ]);
+        return $pdf->download('invoice.pdf');
 
-      $notification = array(
-			'message' => 'Order Delivered Successfully',
-			'alert-type' => 'success'
-		);
-
-		return redirect()->route('shipped-orders')->with($notification);
-
-
-	}
-
-
-	public function AdminInvoiceDownload($order_id){
-
-		$order = Order::with('division','district','state','user')->where('id',$order_id)->first();
-    	$orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
-
-		$pdf = PDF::loadView('backend.orders.order_invoice',compact('order','orderItem'))->setPaper('a4')->setOptions([
-				'tempDir' => public_path(),
-				'chroot' => public_path(),
-		]);
-		return $pdf->download('invoice.pdf');
-
-	}
-
+    }
 
 
 }
